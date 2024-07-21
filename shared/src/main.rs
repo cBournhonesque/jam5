fe0710b1442301a8cd74079da099841ce3640ee9
bokubeam <bokubeam@gmail.com>
2024-07-20 20:26:14 -0600

    Remove unused main in shared library to help rust-analyzer

--- shared/src/main.rs
+++
@@ -1,9 +0,0 @@
-// Disable console on Windows for non-dev builds.
-#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
-
-use bevy::prelude::*;
-use shared::AppPlugin;
-
-fn main() -> AppExit {
-    App::new().add_plugins(AppPlugin).run()
-}
