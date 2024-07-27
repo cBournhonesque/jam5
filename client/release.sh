7ac3c53d5beeff22aee68c1e5bb4a7d7af542a92
cBournhonesque <charlesbour@gmail.com>
2024-07-27 02:33:46 -0400

    add native releases

--- client/release.sh
+++ client/release.sh
@@ -1,3 +1,3 @@
 trunk build --release --dist target
 zip --recurse-paths 'cycles_io-web.zip' 'target'
-BUTLER_API_KEY=FGBKDjQgkMCcqM4PHwBdJ9Vj9NX0Bl3ZpLex1B1s ./butler push --fix-permissions --userversion=v0.0.3 cycles_io-web.zip cbournhonesque/cyclesio:web
+BUTLER_API_KEY=FGBKDjQgkMCcqM4PHwBdJ9Vj9NX0Bl3ZpLex1B1s ./butler push --fix-permissions --userversion=v0.0.5 cycles_io-web.zip cbournhonesque/cyclesio:web
